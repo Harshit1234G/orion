422455f4bb4da83ac07c97e7bc55345ec45ba700
Harshit <harshitkumawat849@gmail.com>
2026-07-24 20:36:40 +0530

    Added a simple interrupt mechanism in tts

--- core/voice_generation.py
+++ core/voice_generation.py
@@ -18,30 +18,34 @@ class PiperVoiceThreadedTTS:
         voice_path = Path(voices_dir) / f'{voice}.onnx'
         self.voice = PiperVoice.load(voice_path, use_cuda= use_cuda)
         self.queue = Queue(maxsize= queue_size)
+        self._STOP = object()
+        self.stop_requested = False
 
-        # daemon is for low priority thread
-        self.worker = Thread(target= self.__tts_worker, daemon= True)
+        self.worker = Thread(target= self.__tts_worker)
         self.worker.start()
 
-        # sentinal
-        self._STOP = object()
 
     def __tts_worker(self) -> None:
         stream = sd.OutputStream(
             samplerate= self.voice.config.sample_rate,
             channels= 1,
-            dtype= 'int16'
+            dtype= 'int16',
+            blocksize= 0,
+            latency= 'low'
         )
         stream.start()
     
         while True:
             try:
-                text = self.queue.get()
+                request = self.queue.get()
         
-                if text is self._STOP:
+                if request is self._STOP:
+                    break
+
+                if self.stop_requested and request.interrupt:
                     break
         
-                for audio_chunk in self.voice.synthesize(text):
+                for audio_chunk in self.voice.synthesize(request.text):
                     stream.write(audio_chunk.audio_int16_array)
 
             except Exception as e:
@@ -51,8 +55,14 @@ class PiperVoiceThreadedTTS:
         stream.close()
 
     # wrapper / helper function for simple calling of tts
-    def say(self, text: str) -> None:
-        self.queue.put(text)
+    def say(
+            self, 
+            text: str, 
+            *,
+            interrupt: bool = True
+        ) -> None:
+        request = SpeechRequest(text, interrupt)
+        self.queue.put(request)
 
     def stop(self) -> None:
         self.queue.put(self._STOP)
@@ -62,6 +72,5 @@ class PiperVoiceThreadedTTS:
 @dataclass
 class SpeechRequest:
     text: str
-    priority: int
-    interrupt: bool = True
+    interrupt: bool
         
