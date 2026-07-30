07b810bfaeb658d0f8e1e3094e2bb57afc694192
Harshit <harshitkumawat849@gmail.com>
2026-07-30 16:15:55 +0530

    Implemented SoundDeviceRecorder

--- voice_engine/speech_to_text.py
+++ voice_engine/speech_to_text.py
@@ -8,7 +8,13 @@ import faster_whisper as fw
 # Abstract base classes
 # ----------------------------
 class BaseRecorder(ABC):
-    ...
+    @abstractmethod
+    def record(self) -> None:
+        ...
+
+    @abstractmethod
+    def stop(self) -> None:
+        ...
 
 
 class BaseVAD(ABC):
@@ -22,8 +28,23 @@ class BaseRecognizer(ABC):
 # ----------------------------
 # Recorder
 # ----------------------------
-class MicrophoneRecorder(BaseRecorder):
-    ...
+class SoundDeviceRecorder(BaseRecorder):
+    def __init__(self, samplerate: int) -> None:
+        self.stream = sd.InputStream(
+            samplerate= samplerate,
+            blocksize= 0,
+            channels= 1,
+            dtype= 'int16',
+            latency= 'low'
+        )
+        self.stream.start()
+
+    def record(self, chunk) -> None:
+        self.stream.read(chunk)
+
+    def stop(self) -> None:
+        self.stream.stop()
+        self.stream.close()
 
 
 # ----------------------------
