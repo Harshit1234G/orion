f4440b612c7b9a2d67feb53aff4358abe4cd6a66
Harshit <harshitkumawat849@gmail.com>
2026-08-09 20:29:56 +0530

    Implemented a simple OpenAI Client

--- orion_engine/llm_api.py
+++ orion_engine/llm_api.py
@@ -1,7 +1,8 @@
 from enum import Enum
 from dataclasses import dataclass
 from openai import OpenAI
-from utils import logger
+
+from utils import logger, load_api_key_from_env, load_api_key_keyring
 
 
 # ----------------------------
@@ -25,7 +26,31 @@ class LLMRequirements:
 # Main Classes
 # ----------------------------
 class OpenAIClient:
-    ...
+    def __init__(self):
+        api_key = load_api_key_from_env() or load_api_key_keyring()
+
+        if api_key is None:
+            logger.error('No OpenAI API key found. OpenAIClient crashed.')
+            raise ValueError('No OpenAI API key found. OpenAIClient crashed.')
+        
+        self.client = OpenAI(api_key= api_key)
+        logger.info('OpenAIClient loaded successfully')
+
+    def generate(
+        self,
+        *,
+        model: OpenAIModel,
+        messages: list[dict],
+        temperature: float,
+        **kwargs
+    ):
+        response = self.client.chat.completions.create(
+            model= model,
+            messages= messages,
+            temperature= temperature,
+            **kwargs
+        )
+        return response
 
 
 class APIRouter:
