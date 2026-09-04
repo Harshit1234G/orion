57038a8635d3bdd555bf74cc80a344b8c5c86f01
Harshit1234G <harshitkumawat849@gmail.com>
2026-09-04 20:19:49 +0530

    Added docstrings, and tested file skills

--- skills/file_system.py
+++ skills/file_system.py
@@ -6,6 +6,9 @@ tm = ToolManager()
 
 @tm.tool
 class FileSystem:
+    """
+    Tools for reading, writing, or creating files and directories.
+    """
     def __init__(self, root: str | Path):
         self.root = Path(root).expanduser().resolve()
 
@@ -20,6 +23,14 @@ class FileSystem:
         return target
 
     def list_directory(self, path: str = '.') -> list[str]:
+        """
+        Lists the content of the directory.
+
+        Args:
+            path: Path is relative to the root directory. Defaults to '.'.
+
+        Returns: A list of all items in directory.
+        """
         target = self._resolve(path)
 
         if not target.is_dir():
@@ -31,6 +42,14 @@ class FileSystem:
         ]
 
     def read_file(self, path: str) -> str:
+        """
+        Reads the content of a text file.
+
+        Args:
+            path: Path is relative to the root directory.
+
+        Returns: Content of the text file.
+        """
         target = self._resolve(path)
 
         if not target.is_file():
@@ -39,6 +58,15 @@ class FileSystem:
         return target.read_text(encoding= 'utf-8')
 
     def write_file(self, path: str, content: str) -> str:
+        """
+        Writes text to the given file, also creates the directory if it doesn't exists.
+
+        Args:
+            path: Path is relative to the root directory.
+            content: Content to write in the file.
+
+        Returns: A confirmation message.
+        """
         target = self._resolve(path)
 
         target.parent.mkdir(parents= True, exist_ok= True)
@@ -47,8 +75,22 @@ class FileSystem:
         return f'File written successfully: {path}'
 
     def create_directory(self, path: str) -> str:
+        """
+        Creates a single directory or nested directories.
+
+        Args:
+            path: Path is relative to the root directory.
+
+        Returns: A confimation message.
+        """
         target = self._resolve(path)
 
         target.mkdir(parents= True, exist_ok= True)
 
         return f'Directory created successfully: {path}'
+
+    def get_root_directory(self) -> str:
+        """
+        Returns the root directory in which LLM has access for file & directory related tasks.
+        """
+        return str(self.root)
