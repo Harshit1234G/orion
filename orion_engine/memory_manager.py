25df33d2f190c5ae3f81e228153794206c1b4b51
Harshit1234G <harshitkumawat849@gmail.com>
2026-09-08 14:11:11 +0530

    Updated memory manager structure

--- orion_engine/memory_manager.py
+++ orion_engine/memory_manager.py
@@ -1,17 +1,39 @@
 from abc import ABC, abstractmethod
-from utils import DatabaseConnector
+from typing import Any
+from utils import DatabaseConnector, logger
 
 
+LOGGING_NAME = '[MemoryManager]'
+
+
+# ------------------
+# Abstract class
+# ------------------
 class Memory(ABC):
     @abstractmethod
-    def save() -> None:
+    def create_table(*args, **kwargs) -> None:
         ...
 
     @abstractmethod
-    def retrieve() -> ...:
+    def delete_table(*args, **kwargs) -> None:
+        ...
+
+    @abstractmethod
+    def save(*args, **kwargs) -> None:
+        ...
+
+    @abstractmethod
+    def retrieve(*args, **kwargs) -> Any:
+        ...
+
+    @abstractmethod
+    def delete(*args, **kwargs) -> None:
         ...
 
 
+# ------------------
+# Main classes
+# ------------------
 class ConversationMemory(Memory):
     ...
 
